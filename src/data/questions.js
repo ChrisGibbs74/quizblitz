50de050ceaea401f001471b9e524da7d743d3ad1
ChrisGibbs74 <crgibbs@wisc.edu>
2026-03-11 10:20:48 +0100

    Extract Questions from data file

---
+++ src/data/questions.js
@@ -0,0 +1,72 @@
+export const questions = [
+  {
+    question: 'What does CSS stand for?',
+    answers: [
+      'Cascading Style Sheets',
+      'Creative Style System',
+      'Computer Style Syntax',
+      'Coloured Screen Sheets',
+    ],
+    correct: 0,
+  },
+  {
+    question: 'Which HTML tag is used to link an external CSS file?',
+    answers: [
+      '<style>',
+      '<css>',
+      '<link>',
+      '<script>',
+    ],
+    correct: 2,
+  },
+  {
+    question: 'Which planet is closest to the Sun?',
+    answers: ['Venus', 'Earth', 'Mercury', 'Mars'],
+    correct: 2,
+  },
+  {
+    question: 'What is the chemical symbol for gold?',
+    answers: ['Ag', 'Au', 'Fe', 'Gd'],
+    correct: 1,
+  },
+  {
+    question: 'How many sides does a heptagon have?',
+    answers: ['5', '6', '8', '7'],
+    correct: 3,
+  },
+  {
+    question: 'Who painted the Mona Lisa?',
+    answers: ['Michelangelo', 'Raphael', 'Leonardo da Vinci', 'Caravaggio'],
+    correct: 2,
+  },
+  {
+    question: 'What is the largest ocean on Earth?',
+    answers: ['Atlantic', 'Indian', 'Arctic', 'Pacific'],
+    correct: 3,
+  },
+  {
+    question: 'In which year did the Berlin Wall fall?',
+    answers: ['1987', '1991', '1989', '1993'],
+    correct: 2,
+  },
+  {
+    question: 'What is the speed of light (approx.) in km/s?',
+    answers: ['150,000', '300,000', '450,000', '1,000,000'],
+    correct: 1,
+  },
+  {
+    question: 'Which element has atomic number 1?',
+    answers: ['Helium', 'Oxygen', 'Hydrogen', 'Carbon'],
+    correct: 2,
+  },
+  {
+    question: 'What language is Django written in?',
+    answers: ['Ruby', 'JavaScript', 'Go', 'Python'],
+    correct: 3,
+  },
+  {
+    question: 'How many bones are in the adult human body?',
+    answers: ['196', '206', '216', '226'],
+    correct: 1,
+  },
+]
